7f811581c66ca55b68b5b5cee7a7ef7dae6e7df7
SIVARANJAN M P <sivaranjanmp0@gmail.com>
2026-08-29 20:22:26 +0530

    Time: 7 ms (7.59%) | Memory: 12.4 MB (57.10%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -5,7 +5,7 @@ class Solution(object):
         for i in range(n):
             for j in range(0, n - i - 1):
                 if nums[j] > nums[j + 1]:
-            
+            # Swap
                     nums[j], nums[j + 1] = nums[j + 1], nums[j]
 
         return nums
